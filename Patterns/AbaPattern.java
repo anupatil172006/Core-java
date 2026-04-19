91c178b7bb6e0d3b5fb862bc08734e654d2afa82
Anushka Patil <anupatil172006@gmail.com>
2026-04-19 22:58:20 +0530

    Add files via upload

---
+++ Patterns/AbaPattern.java
@@ -0,0 +1,54 @@
+/*
+   A
+  ABA
+ ABCBA
+ABCDCBA
+ ABCBA
+  ABA
+   A
+ */
+
+import java.util.Scanner;
+class AbaPattern 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of rows:");
+		int n=sc.nextInt();
+		int space=n/2,pt=1;
+		for(int i=0;i<n;i++)
+		{
+			char ch='A';
+			for(int j=0;j<space;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int k=0;k<pt;k++)
+			{
+				System.out.print(ch);
+			
+				if(k<pt/2)
+				{
+					ch++;
+				}
+				else
+				{
+					ch--;
+				}
+			}
+		
+			if(i<n/2)
+			{
+				space--;
+				pt+=2;
+			}
+			else
+			{
+				space++;
+				pt-=2;
+			}
+			System.out.println();
+		}
+	}
+}
