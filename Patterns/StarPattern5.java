51740537c3a89cd5a235f53b15042ce858084051
Anushka Patil <anupatil172006@gmail.com>
2026-04-25 22:14:10 +0530

    Add files via upload

---
+++ Patterns/StarPattern5.java
@@ -0,0 +1,34 @@
+/*
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+*/
+
+import java.util.Scanner;
+class StarPattern5 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number of rows:");
+		int n =sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(j<n-1-i)
+				{
+					System.out.print(" ");
+				}
+				
+				else
+				{
+					System.out.print("* ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
