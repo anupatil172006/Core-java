dd09b58d0c5e13b2404d1e04129e8768c286ab70
Anushka Patil <anupatil172006@gmail.com>
2026-04-27 21:37:13 +0530

    Add files via upload

---
+++ Patterns/NumberPattern2.java
@@ -0,0 +1,34 @@
+/*
+    1
+   2  3
+  4  5  6
+ 7  8  9  10
+*/
+
+import java.util.Scanner;
+class NumberPattern2 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number of rows:");
+		int n=sc.nextInt();
+		int num=1;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(j<n-1-i)
+				{
+					System.out.print(" ");
+				}
+				else
+				{
+					System.out.print(" "+num+" ");
+					num++;
+				}
+			}
+			System.out.println();
+		}
+	}
+}
