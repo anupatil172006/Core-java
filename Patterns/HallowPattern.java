d1d5e882b6d24683182b3d46a240cf562059176a
Anushka Patil <anupatil172006@gmail.com>
2026-05-02 22:06:14 +0530

    Add files via upload

---
+++ Patterns/HallowPattern.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+class HallowPattern 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of rows:");
+		int n=sc.nextInt();
+		int left=n/2,right=n/2;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(j==left||j==right)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			if(i<n/2)
+			{
+				left--;
+				right++;
+			}
+			else
+			{
+				left++;
+				right--;
+			}
+			System.out.println();
+		}
+	}
+}
+
+/*
+
+    *
+  *   *
+*       *
+  *   *
+    *
+
+
+
+*/
